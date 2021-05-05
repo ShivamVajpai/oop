ce6b98edaae836d1fb53ba77afa005b12e37e19b
Vajpai, Shivam (sv677n) <sv677n@att.com>
2021-05-05 21:42:25 +0530

    Getters and setters tested with multiple types of constructors

---
+++ src/main/java/org/shivam/oop/lesson1/GetterSetterExample.java
@@ -0,0 +1,32 @@
+package main.java.org.shivam.oop.lesson1;
+
+public class GetterSetterExample {
+
+    private float height;
+    private float weight;
+
+    public GetterSetterExample() {}
+
+    public GetterSetterExample(float height, float weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+
+}
