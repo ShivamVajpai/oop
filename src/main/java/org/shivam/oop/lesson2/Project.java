4c05a4bf4c6e36ec2906e23f4d3b5617b5a15e28
Vajpai, Shivam (sv677n) <sv677n@att.com>
2021-05-06 13:03:26 +0530

    Instantiated Hero

---
+++ src/main/java/org/shivam/oop/lesson2/Project.java
@@ -0,0 +1,11 @@
+package main.java.org.shivam.oop.lesson2;
+
+public class Project {
+    public static void main(String[] args) {
+        Hero h1 = new Hero("Big Man");
+        h1.showAbilities();
+        h1.fight();
+        h1.run();
+
+    }
+}
--- src/main/java/org/shivam/oop/lesson2/Thursday.java
+++
@@ -1,7 +0,0 @@
-package main.java.org.shivam.oop.lesson2;
-
-public class Thursday {
-    public static void main(String[] args) {
-
-    }
-}
